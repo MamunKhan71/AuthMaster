597f115a8d5868385f9d76e4bd9bed37a371a89f
Md. Mamun <mkmamun031@gmail.com>
2024-08-24 20:45:05 +0600

    Added Email Verification System

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
 import { generateTokenSetCookie } from "../utils/generateTokenSetCookies.js"
-import { sendVerificationEmail } from "../maitrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../maitrap/emails.js";
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
@@ -37,6 +37,27 @@ export const signup = async (req, res) => {
         })
     } catch (error) {
         res.status(400).json({ success: false, message: error.message })
+    }
+}
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() }
+        })
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired verification code" })
+        }
+        user.isVerified = true
+        user.verificationToken = undefined
+        user.verificationTokenExpiresAt = undefined
+        await user.save()
+        await sendWelcomeEmail(user.email, user.name)
+        res.status(200).json({ succcess: true, message: "Email Verified Succesfully!", user: { ...user._doc, password: undefined } })
+    } catch (error) {
+        console.log("Error: ", error)
+        res.status(500).json({ success: false, message: "Server Error" })
     }
 }
 export const login = async (req, res) => {
--- Backend/maitrap/emails.js
+++ Backend/maitrap/emails.js
@@ -17,3 +17,21 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending : ${error}`)
     }
 }
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }]
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "756a13b6-412d-4feb-833a-94e8b4f635aa",
+            template_variables: {
+                "company_info_name": "AuthSuite",
+                "name": name,
+            }
+        })
+        console.log("Welcome Email sent successfully ", response);
+    } catch (error) {
+        console.log(`Error Sending Email : ${error}`)
+        throw new Error(`Error Sending Email : ${error}`)
+    }
+}
