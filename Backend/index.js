aac97933cd7f03b1385c7bc70792fbfb09d81465
Md. Mamun <mkmamun031@gmail.com>
2024-08-24 15:16:07 +0600

    Added MailTrap For email sending purpose

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './db/connectDB.js'
 const app = express()
+const port = process.env.PORT || 5000
 dotenv.config()
+import authRoutes from './routes/auth_route.js'
 app.get('/', async (req, res) => {
     res.send('Running Successfully!')
 })
-app.listen(3000, () => {
+app.use(express.json())
+app.use('/api/auth', authRoutes)
+app.listen(port, () => {
     connectDB()
-    console.log("Server is running on port: 3000");
+    console.log("Server is running on port: ", port);
 })
